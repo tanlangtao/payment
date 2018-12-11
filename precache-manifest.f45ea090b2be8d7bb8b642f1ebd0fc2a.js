748233f19c41c34a064d3f4ed6425000a481be01
Andy <924115911@qq.com>
2018-12-11 16:46:49 +0800

    first commit

---
+++ precache-manifest.f45ea090b2be8d7bb8b642f1ebd0fc2a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "17e3f8f11043f39bf967",
+    "url": "./static/css/main.a695f181.chunk.css"
+  },
+  {
+    "revision": "17e3f8f11043f39bf967",
+    "url": "./static/js/main.17e3f8f1.chunk.js"
+  },
+  {
+    "revision": "83d7799b8f16becdc3a1",
+    "url": "./static/js/1.83d7799b.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "c4f2a1f467f8da463ac7ee8194fe19ce",
+    "url": "./index.html"
+  }
+];
