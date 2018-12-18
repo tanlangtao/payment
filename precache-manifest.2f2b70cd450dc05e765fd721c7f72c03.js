5938409b656eff52dc5ac72ad168b32e35e230d8
Andy <924115911@qq.com>
2018-12-18 15:16:25 +0800

    Add files via upload

---
+++ precache-manifest.2f2b70cd450dc05e765fd721c7f72c03.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae86caa0467056a05093",
+    "url": "./static/css/main.4299598c.chunk.css"
+  },
+  {
+    "revision": "ae86caa0467056a05093",
+    "url": "./static/js/main.ae86caa0.chunk.js"
+  },
+  {
+    "revision": "83d7799b8f16becdc3a1",
+    "url": "./static/js/1.83d7799b.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "8e2c3b1e56d7f1e469bc1cab61b00b23",
+    "url": "./index.html"
+  }
+];
