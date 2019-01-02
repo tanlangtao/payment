21673468d563d451baa07b5cdb343b3fde0a53b7
Andy <924115911@qq.com>
2019-01-02 10:55:04 +0800

    Add files via upload

---
+++ precache-manifest.9f6c9b17a120f5c704708313918fbcd1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e61038e0c1aeab6d11fd",
+    "url": "./static/css/main.b253513d.chunk.css"
+  },
+  {
+    "revision": "e61038e0c1aeab6d11fd",
+    "url": "./static/js/main.e61038e0.chunk.js"
+  },
+  {
+    "revision": "d85dabe3519dfd1759d2",
+    "url": "./static/js/1.d85dabe3.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "f92f5e3b523f405a106e4eb9c247c0c6",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.8b84d6ba88cf1a5b161efd5ae7d836e4.js"
+  "./precache-manifest.9f6c9b17a120f5c704708313918fbcd1.js"
 );
 
 workbox.clientsClaim();
